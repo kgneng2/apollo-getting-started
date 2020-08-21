3a7685491fdf8ce04b975b902e0c8669a8bc7dbb
junyoung-kang <junyoung.kang@navercorp.com>
2020-08-21 16:05:48 +0900

    -ing

---
+++ start/server/src/schema.js
@@ -0,0 +1,40 @@
+const { gql } = require('apollo-server');
+
+const typeDef = gql`
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+  
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+  
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+  
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+`
+module.exports= typeDef;
